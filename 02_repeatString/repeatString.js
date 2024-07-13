c618adb040e3a51def11881da888970376e4acb8
Rishab Jha <108749503+RishabJha@users.noreply.github.com>
2024-07-13 12:04:09 +0530

    Revert "completed question 1"
    This reverts commit 092edd52748d4f74f9405668884f997a57dd4d98.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,22 +1,5 @@
-const repeatString = function (word, times) {
-    if (times < 0) {
-        return 'ERROR'
-    }
+const repeatString = function() {
 
-    if (word === '') {
-        return ''
-    }
-
-    if (times == 0) {
-        return ""
-    }
-
-    let str = ''
-    for (let i = 0; i < times; i++) {
-        str += word
-    }
-
-    return str
 };
 
 // Do not edit below this line
