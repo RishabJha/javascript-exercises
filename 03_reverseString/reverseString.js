e82fb6843002de9b7cbb9a46f66042fc3fccf71f
Rishab Jha <108749503+RishabJha@users.noreply.github.com>
2024-07-13 12:06:27 +0530

    completed question 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+    if (word === '') {
+        return ''
+    }
+    let ans = ''
+    for (let i = word.length - 1; i >= 0; i--) {
+        ans += word.charAt(i)
+    }
+    return ans
 };
 
 // Do not edit below this line
