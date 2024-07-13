092edd52748d4f74f9405668884f997a57dd4d98
Rishab Jha <108749503+RishabJha@users.noreply.github.com>
2024-07-13 12:03:41 +0530

    completed question 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+    if (times < 0) {
+        return 'ERROR'
+    }
 
+    if (word === '') {
+        return ''
+    }
+
+    if (times == 0) {
+        return ""
+    }
+
+    let str = ''
+    for (let i = 0; i < times; i++) {
+        str += word
+    }
+
+    return str
 };
 
 // Do not edit below this line
