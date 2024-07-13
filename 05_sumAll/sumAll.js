b20642e46cedbb689f0032512ed1f236d3d96de7
Rishab Jha <108749503+RishabJha@users.noreply.github.com>
2024-07-13 12:40:37 +0530

    completed question 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
 
+    if (start < 0 || end < 0 || typeof start !== 'number' || typeof end != 'number') {
+        return 'ERROR'
+    }
+
+    let ans = 0
+    if (start > end) {
+        const temp = start
+        start = end
+        end = temp
+    }
+    for (let i = start; i <= end; i++) {
+        ans += i
+    }
+    return ans
 };
 
 // Do not edit below this line
