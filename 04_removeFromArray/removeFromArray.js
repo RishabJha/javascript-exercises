1ef209be434b2ac23ce12854ec74351e4cc4743f
Rishab Jha <108749503+RishabJha@users.noreply.github.com>
2024-07-13 12:21:12 +0530

    completed question 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...items) {
+    newArray = []
+    array.forEach(element => {
+        if (!items.includes(element)) {
+            newArray.push(element)
+        }
+    });
+
+    return newArray
 };
 
 // Do not edit below this line
