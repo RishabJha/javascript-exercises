e8be47bec400c9472ed7c4a3ddcee524f60a3ad0
Rishab Jha <108749503+RishabJha@users.noreply.github.com>
2024-07-17 14:56:59 +0530

    completed question 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+function getDeath(birth, death) {
+    if (!death) {
+        death = (new Date()).getFullYear()
+        return death - birth
+    }
+    return death - birth
+}
 
+const findTheOldest = function (array) {
+    const sortArray = array.sort((first, second) => {
+        const a = getDeath(first.yearOfBirth, first.yearOfDeath)
+        const b = getDeath(second.yearOfBirth, second.yearOfDeath)
+        if (a > b) {
+            return -1
+        } else {
+            return 1
+        }
+    })
+
+    return sortArray[0]
 };
 
 // Do not edit below this line
