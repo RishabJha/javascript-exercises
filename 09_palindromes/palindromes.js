02dd786fe1e4c8b690ab7d5ea961a680cc11b0c4
Rishab Jha <108749503+RishabJha@users.noreply.github.com>
2024-07-17 14:00:42 +0530

    completed question 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    inputString = inputString.toLowerCase
+    const n = inputString.length;
+    for (let i = 0; i < Math.floor(n / 2); i++) {
+        if (inputString.at(i) !== inputString.at(n - 1 - i)) {
+            return false
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
