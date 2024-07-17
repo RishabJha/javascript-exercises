ad19cc4f76d2b84a7191a5d8fa54e9b0fb54de07
Rishab Jha <108749503+RishabJha@users.noreply.github.com>
2024-07-17 14:14:04 +0530

    completed question 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    num = Number(num)
+    let sum = 1
+    if (num < 0) {
+        return "OOPS"
+    }
+    if (num <= 1) {
+        return num
+    }
+    if (Number.isInteger(num)) {
+        let prev = 0
+        let curr = 1
+        for (let i = 1; i < num; i++) {
+            sum = curr + prev
+            prev = curr
+            curr = sum
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line
